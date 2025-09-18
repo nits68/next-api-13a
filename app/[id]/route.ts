da57d8f4e81ab9a553e3f7ebcd7627e33c9fb5a4
nits68 <nits.laszlo@jedlik.eu>
2025-09-18 13:10:59 +0200

    first commit

---
+++ app/[id]/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  return NextResponse.json({ message: `Hello ${id}!` });
+}
